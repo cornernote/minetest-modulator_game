09144b2a9b45dd2bb57a4f1f234ca624fbd36ead
Brett O'Donnell <cornernote@gmail.com>
2012-09-21 19:31:48 +0930

    update bones

--- mods/effects/bones/init.lua
+++ mods/effects/bones/init.lua
@@ -1,5 +1,5 @@
 ---
---Bones 1.01
+--Bones 1.02
 --Copyright (C) 2012 Bad_Command
 --
 --This library is free software; you can redistribute it and/or
@@ -18,7 +18,7 @@
 ----
 
 bones = {}
-bones.version = 1.01
+bones.version = 1.02
 
 -- config.lua contains configuration parameters
 dofile(minetest.get_modpath("bones").."/config.lua")
@@ -27,7 +27,7 @@ dofile(minetest.get_modpath("bones").."/bones.lua")
 
 minetest.register_node("bones:bones", {
 	description = "Old Bones",
-	tiles ={"bones_top.png", "bones_bottom.png", "bones_side.png",
+	tile_images ={"bones_top.png", "bones_bottom.png", "bones_side.png",
 		"bones_side.png", "bones_rear.png", "bones_front.png"},
 	paramtype2 = "facedir",
 	groups = {crumbly=2},
@@ -44,13 +44,14 @@ minetest.register_node("bones:bones", {
 		local inv = meta:get_inventory()
 		return inv == nil or inv:is_empty("main")
 	end,
-	on_metadata_inventory_move = function(pos, from_list, from_index,
-			to_list, to_index, count, player)
+	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
+		return 0
 	end,
-	on_metadata_inventory_offer = function(pos, listname, index, stack, player)
-		return stack
+	allow_metadata_inventory_move = function(pos, listname, index, stack, player)
+		return 0
 	end,
-   	on_metadata_inventory_take = bones.inventory_take,
+	allow_metadata_inventory_take = bones.allow_inventory_take,
+   	on_metadata_inventory_take = bones.inventory_take
 })
 
 minetest.register_abm(
@@ -61,4 +62,5 @@ minetest.register_abm(
 })
 
 minetest.register_on_dieplayer(bones.on_dieplayer)
+minetest.register_on_punchnode(bones.on_punch)
 
